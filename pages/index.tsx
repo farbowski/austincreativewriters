8f6347ca093f8a187fac5a6058cbd7a6c0d2e38a
farbowski <111902880+farbowski@users.noreply.github.com>
2022-08-24 16:52:50 -0500

    Extended

--- pages/index.tsx
+++ pages/index.tsx
@@ -112,10 +112,10 @@ const Home: NextPage = () => {
                 ✍️ I&apos;m a complete beginner. Can I still join?
               </p>
               <p>We welcome all skill levels.</p>
-              <p className={styles.faqQuestion}>✍️ Will u fix my grammer?</p>
+              <p className={styles.faqQuestion}>✍️ Will you fix my grammer?</p>
               <p>Nope.</p>
               <p className={styles.faqQuestion}>
-                ✍️ But seriously, my grammar sucks.
+                ✍️ But seriously, my grammar sucks. I didn't even notice that grammar was misspelled in the previous question.
               </p>
               <p>
                 A typo here and there is fine but your submission should be
